0a5ed9cda41d17a97403272782fc45966af1c308
Pablo Fontecilla <pafonbu@gmail.com>
2021-07-19 14:29:11 -0400

    HRAP-110 FIX: getmensajes a getMensajes

--- api/controllers/documentosController.js
+++ api/controllers/documentosController.js
@@ -16,6 +16,6 @@ exports.getDocumentos = async (req, res) => {
       .exec();
     res.status(200).send(documentos);
   } catch (error) {
-    res.status(500).send({ respuesta: await getmensajes("serverError") });
+    res.status(500).send({ respuesta: await getMensajes("serverError") });
   }
 };
