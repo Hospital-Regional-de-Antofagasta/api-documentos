1c648609e25b38a5a8d5e2bcfa8a00ec749e4d5c
pafonbu <pafonbu@gmail.com>
2022-06-08 15:25:22 -0400

    audit fix

--- api/models/ConfigApiDocumentos.js
+++ api/models/ConfigApiDocumentos.js
@@ -3,41 +3,44 @@ const Schema = mongoose.Schema;
 
 const ConfigApiDocumentos = mongoose.model(
   "config_api_documentos",
-  new Schema({
-    mensajes: {
-      forbiddenAccess: {
-        titulo: String,
-        mensaje: String,
-        color: String,
-        icono: String,
+  new Schema(
+    {
+      mensajes: {
+        forbiddenAccess: {
+          titulo: String,
+          mensaje: String,
+          color: String,
+          icono: String,
+        },
+        serverError: {
+          titulo: String,
+          mensaje: String,
+          color: String,
+          icono: String,
+        },
+        badRequest: {
+          titulo: String,
+          mensaje: String,
+          color: String,
+          icono: String,
+        },
+        solicitudCreada: {
+          titulo: String,
+          mensaje: String,
+          color: String,
+          icono: String,
+        },
+        solicitudDuplicada: {
+          titulo: String,
+          mensaje: String,
+          color: String,
+          icono: String,
+        },
       },
-      serverError: {
-        titulo: String,
-        mensaje: String,
-        color: String,
-        icono: String,
-      },
-      badRequest: {
-        titulo: String,
-        mensaje: String,
-        color: String,
-        icono: String,
-      },
-      solicitudCreada: {
-        titulo: String,
-        mensaje: String,
-        color: String,
-        icono: String,
-      },
-      solicitudDuplicada: {
-        titulo: String,
-        mensaje: String,
-        color: String,
-        icono: String,
-      },
+      version: Number,
     },
-    version: Number,
-  }),
+    { timestamps: true }
+  ),
   "config_api_documentos"
 );
 
